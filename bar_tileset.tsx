b227ee66405ad522ac9c62cff0446b074de540b3
jofraesh <84086330+jofraesh@users.noreply.github.com>
2021-05-13 17:23:22 +0200

    *added test_map_1

---
+++ bar_tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="bar_tileset" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="../../../Desktop/Cellu L´art/CineVenture/TIles/bar_tileset_by_aveontrainer_ddpy4ut.png" trans="ff00ff" width="256" height="384"/>
+ <tile id="91">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
